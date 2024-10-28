422665504356f292afe29831bae2e50053b39dfd
Iain Cameron <Ca3822Ia0216@pusd.us>
2024-10-28 10:32:42 -0700

    sprint B

--- level_2.gd
+++ level_2.gd
@@ -1,21 +1,14 @@
 extends Area2D
 
-var speed = 5
-var velocity = Vector2()
-#var moving = True
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = Vector2(-speed,0)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	translate(velocity)
+	pass
 
-#if moving == True
-	#velocity.x = speed
-	
 func _on_body_entered(body):
-	if body.has_method("move_back"):
-		body.move_back(12, 318)
+	print("hi")
--- objects/player.gd
+++ objects/player.gd
@@ -16,10 +16,14 @@ func _physics_process(delta: float) -> void:
 		jumps = 0
 		hasDashed = false
 
-	if Input.is_action_just_pressed("ui_accept") and jumps < 2:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():#jumps < 2:
 		velocity.y = JUMP_VELOCITY
 		jumps = jumps + 1
-		
+
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and jumps == 1:
+		velocity.y = JUMP_VELOCITY
+		jumps = jumps + 1
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -28,8 +32,17 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
+	# Dashing
 	if Input.is_action_just_pressed("shift") and hasDashed == false:
 		velocity.x = direction * dashSpeed
 		hasDashed = true
 
 	move_and_slide()
+
+func move_back(x, y):
+	position.x = x
+	position.y = y
+
+#if body.has_method("move_back") and body.position.y > 398:
+	#print("hi")
+	#body.move_back(12, 318)
